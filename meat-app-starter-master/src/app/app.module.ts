d27db98af8d0c6e4f4f7b21b0a5fc28f5524b238
Daniel da Silva de Lima <dsl15021996@gmail.com>
2019-05-14 17:56:19 -0300

    Criando um Componente de Input (Content Projection)

--- meat-app-starter-master/src/app/app.module.ts
+++ meat-app-starter-master/src/app/app.module.ts
@@ -19,7 +19,7 @@ import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shoppin
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
 import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
 import { OrderComponent } from './order/order.component';
-
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -39,7 +39,8 @@ import { OrderComponent } from './order/order.component';
   imports: [
     BrowserModule,
     HttpModule,
-    RouterModule.forRoot(ROUTES)
+    RouterModule.forRoot(ROUTES),
+    FormsModule,
   ],
   providers: [
     RestaurantsService,
