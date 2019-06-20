ebe3e344d601907b8a0f179652c59d7f3fbe482a
Daniel da Silva de Lima <dsl15021996@gmail.com>
2019-06-19 23:54:10 -0300

    75. O que são Módulos? 00:00

--- meat-app-starter-master/src/app/order/order.component.ts
+++ meat-app-starter-master/src/app/order/order.component.ts
@@ -2,14 +2,21 @@ import { Component, OnInit } from '@angular/core';
 import { RadioOption } from 'app/shared/radio/radio-option.model';
 import { OrderService } from './order.service';
 import { CartItem } from 'app/restaurant-detail/shopping-cart/cart-item.model';
-import { OrderItem } from './order.model';
+import { OrderItem, Order } from './order.model';
 import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'mt-order',
   templateUrl: './order.component.html'
 })
 export class OrderComponent implements OnInit {
+
+  emailPattern = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+
+  numberPattern = /^[0-9]*$/
+
+  orderForm: FormGroup;
 
   delivery: number = 8;
 
@@ -21,10 +28,34 @@ export class OrderComponent implements OnInit {
 
   constructor(
     private orderService: OrderService,
-    private route: Router
+    private route: Router,
+    private formBuilder: FormBuilder
   ) { }
 
   ngOnInit() {
+    this.orderForm = this.formBuilder.group({
+      name: this.formBuilder.control('', [Validators.required, Validators.minLength(5)]),
+      email: this.formBuilder.control('', [Validators.required, Validators.pattern(this.emailPattern)]),
+      emailConfirmation: this.formBuilder.control('', [Validators.required, Validators.pattern(this.emailPattern)]),
+      address: this.formBuilder.control('', [Validators.required, Validators.minLength(5)]),
+      number: this.formBuilder.control('', [Validators.required, Validators.pattern(this.numberPattern)]),
+      optionalAddress: this.formBuilder.control(''),
+      paymentOptions: this.formBuilder.control('', [Validators.required])
+    }, { Validators: OrderComponent.equalsTo })
+  }
+
+  static equalsTo(group: AbstractControl): { [key: string]: boolean } {
+    const email = group.get('email');
+    const emailConfirmation = group.get('emailConfirmation');
+
+    if (!email || !emailConfirmation) {
+      return undefined
+    }
+
+    if (email.value !== emailConfirmation.value) {
+      return { teste: true }
+    }
+    return undefined;
   }
 
   itemsValue(): number {
