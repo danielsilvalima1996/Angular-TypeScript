8b1c7f0d5103db68e781cc9718592d6d17710ca6
Daniel da Silva de Lima <dsl15021996@gmail.com>
2019-07-03 00:10:28 -0300

    travei na aula 109

--- meat-app-starter-master/src/app/order/order.service.ts
+++ meat-app-starter-master/src/app/order/order.service.ts
@@ -3,7 +3,7 @@ import { ShoppingCartService } from "app/restaurant-detail/shopping-cart/shoppin
 import { CartItem } from "app/restaurant-detail/shopping-cart/cart-item.model";
 import { Order } from "./order.model";
 import { Observable } from "rxjs/Observable";
-import { Http, Headers, RequestOptions } from "@angular/http";
+import { HttpClient } from "@angular/common/http";
 import { MEAT_API } from "app/app.api";
 
 
@@ -12,7 +12,7 @@ export class OrderService {
 
     constructor(
         private cartService: ShoppingCartService,
-        private http: Http
+        private http: HttpClient
     ) { }
 
     itemsValue(): number {
@@ -36,12 +36,7 @@ export class OrderService {
     }
 
     checkOrder(order: Order): Observable<string> {
-        const headers = new Headers();
-        headers.append('Content-Type', 'application/json');
-        return this.http.post(`${MEAT_API}/orders`,
-            JSON.stringify(order),
-            new RequestOptions({ headers: headers }))
-            .map(response => response.json())
+        return this.http.post<Order>(`${MEAT_API}/orders`, order)
             .map(order => order.id)
     }
 
--- meat-app-starter-master/src/app/restaurants/restaurants.service.ts
+++ meat-app-starter-master/src/app/restaurants/restaurants.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core'
-import { Http } from '@angular/http';
 import { Restaurant } from './restaurant/restaurant.model';
 import { MEAT_API } from '../app.api';
 import { Observable } from 'rxjs/Observable';
@@ -8,12 +7,13 @@ import 'rxjs/add/operator/catch';
 import { ErrorHandler } from '../app.error-handler';
 import { errorHandler } from '@angular/platform-browser/src/browser';
 import { MenuItem } from '../restaurant-detail/menu-item/menu-item.model';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable()
 export class RestaurantsService {
 
   constructor(
-    private http: Http
+    private http: HttpClient
   ) { }
 
   /**
@@ -22,27 +22,26 @@ export class RestaurantsService {
    * response joga no response.json
    */
   restaurants(search?: string): Observable<Restaurant[]> {
-    return this.http.get(`${MEAT_API}/restaurants`, { params: { q: search } })
-      .map(response => response.json())
-      .catch(ErrorHandler.handleError)
+    let params: HttpParams = undefined;
+
+    if (search) {
+      params = new HttpParams().append('q', search);
+    }
+
+    return this.http.get<Restaurant[]>(`${MEAT_API}/restaurants`, { params: params })
+      
   }
 
   restaurantById(id: string): Observable<Restaurant> {
-    return this.http.get(`${MEAT_API}/restaurants/${id}`)
-      .map(response => response.json())
-      .catch(ErrorHandler.handleError)
+    return this.http.get<Restaurant>(`${MEAT_API}/restaurants/${id}`)
   }
 
   reviewsOfRestaurant(id: string): Observable<any> {
-    return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
-      .map(response => response.json())
-      .catch(ErrorHandler.handleError);
+    return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`);
   }
 
   menuOfRestaurant(id: string): Observable<MenuItem[]> {
-    return this.http.get(`${MEAT_API}/restaurants/${id}/menu`)
-      .map(response => response.json())
-      .catch(ErrorHandler.handleError);
+    return this.http.get<MenuItem[]>(`${MEAT_API}/restaurants/${id}/menu`);
   }
 
 }
